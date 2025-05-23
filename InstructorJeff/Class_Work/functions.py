6b55efb35a2b1afa06ae8d2511f09f0489686c61
SelinaMasembe <selinamasembe27@gmail.com>
2025-05-23 09:51:03 +0300

    Used return to get the quotient

--- InstructorJeff/Class_Work/functions.py
+++ InstructorJeff/Class_Work/functions.py
@@ -13,7 +13,8 @@ def greet_student ():
     print("Hello, Student!")
  
 # Call the function
-greet_student()   
+greet_student() 
+print("\n")  
 
 #PEP8 guidelines:
 #Snake case for function names
@@ -26,3 +27,17 @@ def add_numbers(num1,num2):
     print("The sum of",num1,"and", num2,"is:", result)
     
 add_numbers(5,6)
+
+#Return
+#ends the function execution and returns a value
+
+#return multiple values
+def get_stats(a,b):
+    return a+b, a-b, a*b, a/b
+
+# Call the function
+sum,difference,product,quotient = get_stats(10,5)
+print("Sum:", sum)
+print("Difference:", difference)    
+print("Product:", product)
+print("Quotient:", quotient)
---
+++ InstructorJeff/In_Class_Exercises/return_to_divide.py
@@ -0,0 +1,5 @@
+def quotient(a, b):
+    return a /b
+
+result = quotient(10, 2)
+print("The quotient of 10 and 2 is:", result)
