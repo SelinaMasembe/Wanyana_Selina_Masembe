2d5b57082d0c0433e7277c7ae9e03508fb38b350
SelinaMasembe <selinamasembe27@gmail.com>
2025-06-07 11:17:15 +0300

    I have downloaed the dataset

---
+++ InstructorJeff/Machine_Learning/learnML/train_model.py
@@ -0,0 +1,5 @@
+#import required libraries
+
+import os
+import numpy as np
+import tensorflow as tf #type: ignore    
---
+++ InstructorJeff/Machine_Learning/learn_machine_learning.py
@@ -0,0 +1,54 @@
+#MACHINE LEARNING
+#What is Machine Learning
+
+"""
+APPLICATION OF MACHINE LEARNING:
+1.Image analysis
+2.Speech Recognition
+3.Disease Prediction
+4.Stock Market Analysis
+5.Fraud detection
+6.Recommendation Systems
+"""
+
+
+"""
+TYPES OF MACHINE LEARNING:
+1.Supervised Learning
+2.Unsupervised Learning
+3.Semi supervised learning- uses both supervised and unsupervised learning
+4.Reinforcement Learning-uses rewards
+"""
+
+#SUPERVISED LEARNING - labelled dataset, structured
+"""
+Common algorithms with Supervised learning:
+.Linear Regression
+.Logistic Regression
+.Decision trees
+.Support vector machine (SVM)
+.K-Nearest Neighbor (KNN)
+.Random Forest(RF)
+
+.Deep learning (CNN ) -image classification
+
+"""
+
+#UNSUPERVISED LEARNING
+"""
+-used on unlabeled data
+-to discover hidden patterns
+-clusters
+-structures
+
+USES:
+customer segmentation
+"""
+
+"""
+COMMON UNSUPERVISED ALGORITHMS
+1.K-means clustering
+2.Hierarchical clustering
+3.PCA-Principal Component Analysis
+"""
+#here we have: Model selection and validation
