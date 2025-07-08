641eca626107401a4fcd0df0bf0bb0ba33fde7fa
SelinaMasembe <selinamasembe27@gmail.com>
2025-07-08 21:53:11 +0300

    I have created my root project

---
+++ InstructorJeff/LEARN_DJANGO/learn_django.py
@@ -0,0 +1,9 @@
+"""
+1.Create virtual environment
+2.Install Django    
+3.Create Django project #Project, App, Templates
+4.Test the server  python manage.py runserver
+5.Create apps
+6.Create templates
+7.Run server a. do migrations b.runserver
+"""
